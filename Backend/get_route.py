2be4fdd548850417826d5ca00104e73d4ba43b01
rohitjg13 <jgrohit.exe@gmail.com>
2025-08-07 23:16:23 +0530

    CORS

--- Backend/get_route.py
+++ Backend/get_route.py
@@ -9,7 +9,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "*",
+        "https://campus-map-optimisation.vercel.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
